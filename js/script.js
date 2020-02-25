8efed2baae42ecab850d48a98952aa1b96c93c98
Ivan Bumbak <bumbak.ivan@gmail.com>
2020-02-25 14:25:16 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Smooth scrolling on clik
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//Show project description on mouseover
